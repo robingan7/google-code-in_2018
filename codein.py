0215f6a6360511daadec658cfd2d6c16559523dc
Robin Gan <xiangyu.gan@robingan.org>
2019-10-06 16:24:20 -0700

    Add files via upload

---
+++ codein.py
@@ -0,0 +1,12 @@
+
+for i in range(10):
+    print("GCI is great")
+
+name=input("Enter your name")
+
+reverse=reversed(name)
+
+reverse=''.join(reverse)
+
+print("Hello "+name+", plaese to meet you!"+
+      "Did you know that your name backwards is "+reverse)
